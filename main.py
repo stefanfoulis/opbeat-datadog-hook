a95e1b2d28e3fb34e16ec9e5594d8ae748877f8d
Stefan Foulis <stefan@foulis.ch>
2014-11-05 08:39:37 +0100

    make it datadog

--- main.py
+++ main.py
@@ -1,58 +1,51 @@
-import json
 import os
-import requests
 from bottle import get, post, run, request
 from bottle import jinja2_template as template
+from dogapi import dog_http_api as api
 
-activity_template = """
-{{app['name']}}: <{{html_url}}|{{title}}>\n{{summary}}
+title_template = """
+{{app['name']}}: {{title}}
+"""
+
+text_template = """
+{{html_url}}
+
+{{summary}}
 """
 
 
-DEFAULT_COLOR = "#808080"
-COLORS = {
-    "errorgroup": {"created": "bad"},
-    "release": {"created": "good"},
-}
-
-
-def get_color(subject_type, action):
-    return COLORS.get(subject_type, {}).get(action, DEFAULT_COLOR)
-
-SLACK_URL = os.environ.get('SLACK_URL')
-if not SLACK_URL:
-    print("Missing environment variable SLACK_URL")
+DATADOG_API_KEY = os.environ.get('DATADOG_API_KEY')
+if not DATADOG_API_KEY:
+    print("Missing environment variable DATADOG_API_KEY")
     exit(1)
+else:
+    api.api_key = DATADOG_API_KEY
+
+
+TAGS = [tag.strip() for tag in os.environ.get('TAGS', "").split(',')]
 
 
 def send(data):
-    rendered_activity = template(activity_template, **data)
-    post_data = {
-        "icon_url": "https://secure.gravatar.com/avatar/a6ec5537afa27bdb4fe5056e2d34810d",
-        "username": "Opbeat",
-        "attachments": [
-            {
-                "fallback": rendered_activity,
-                "pretext": u"{}: <{}|{}>".format(data['app']['name'], data['html_url'], data['title']),
-                "color": get_color(data['subject_type'], data['action']),
-                "fields": [
-                    {
-                        "value": data['summary'],
-                        "short": False
-                    }
-                ]
-            }
-        ]
-    }
-
-    resp = requests.post(
-        SLACK_URL,
-        data={"payload": json.dumps(post_data)}
-    )
-    if resp.status_code != 200:
-        print(resp.status_code, resp.text)
+    title = template(title_template, **data)
+    text = template(text_template, **data)
+    tags = [tag for tag in TAGS if tag]
+    tags.append('app:{}'.format(data['app']['short_name']))
+    tags.append('subject_type:{}'.format(data['subject_type']))
+    tags.append('organization:{}'.format(data['organization']['short_name']))
+    subject_type = data.get('subject_type', '')
+    if subject_type in ['release']:
+        alert_type = 'success'
+    elif subject_type in ['errorgroup']:
+        alert_type = 'error'
     else:
-        print("Sent activity to slack")
+        alert_type = 'info'
+    tags.append(alert_type)
+    api.event_with_response(
+        title,
+        text,
+        tags=tags,
+        alert_type=alert_type,
+    )
 
 
 @post('/new-activity')
